71b5b832b7441240f8490f2da1dae91f56b2b924
Anselem <anselemodims@gmail.com>
2021-11-29 10:18:40 +0100

    Included a start and build script in package.json

---
+++ src/index.js
@@ -0,0 +1,1 @@
+document.querySelector('h2').innerText = "JSON"
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
