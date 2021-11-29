c714c6b2918a71732b2e9c0edb6f193559096441
Anselem <anselemodims@gmail.com>
2021-11-29 10:18:40 +0100

    Included a start and build script in package.json

---
+++ bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document,querySelector('h2').innerText = "JSON"
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsK0MiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudCxxdWVyeVNlbGVjdG9yKCdoMicpLmlubmVyVGV4dCA9IFwiSlNPTlwiIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
