c8cc72896ef00f4f1a806f0f86584a53349dce82
Anselem <anselemodims@gmail.com>
2021-11-29 16:58:30 +0100

    Created a focusHandler instance helper method

--- src/helpers.js
+++ src/helpers.js
@@ -27,4 +27,25 @@ export default class Helper {
       this.checkHandler();
     });
   }
+
+  /**
+   * @function focusHandler - handles the focus on the inputs
+   * @param {Event Object} e - The current event
+   */
+  focusHandler(e) {
+    const item = e.currentTarget;
+    const child = item.children;
+    if (document.querySelector('li.list.focus')) {
+      const parent = document.querySelector('li.list.focus');
+      parent.children[2].classList.remove('focus');
+      parent.children[3].classList.remove('d-none');
+      parent.children[4].classList.add('d-none');
+      parent.classList.remove('focus');
+    }
+    child[2].focus();
+    utils.tog('add', 'focus', child[2]);
+    utils.tog('add', 'd-none', child[3]);
+    utils.tog('rem', 'd-none', child[4]);
+    item.classList.add('focus');
+  }
 }
