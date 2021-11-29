f4eb635ba4a38b545b3de85107a3453ec7c51763
Anselem <anselemodims@gmail.com>
2021-11-29 12:01:06 +0100

    Created the render instance method for the Utils class

--- src/component.js
+++ src/component.js
@@ -1,5 +1,7 @@
-export default function component(val, id) {
-  return `
+/* eslint-disable class-methods-use-this */
+export default class Component {
+  static list(val, id) {
+    return `
   <li class="list">
   <input type="checkbox" id="check" data-id=${id}><img src="check.svg" alt="checked icon" class="d-none" data-id=${id}>
   <input type="text" class="input-value" value='${val}'>
@@ -7,4 +9,5 @@ export default function component(val, id) {
   <a class="d-none"  id="delete"><i class="far fa-trash-alt" data-id=${id}></i></a>
 </li>
   `;
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,13 @@
+/* eslint-disable class-methods-use-this */
 import task from './tasks.js';
-import component from './component.js';
-
+import Component from './component.js';
 
 export default class Utils {
-
+  /**
+   * @function render - instance class method
+   * @returns - a list element formed from the component
+   */
+  render() {
+    return task.map((item) => Component.list(item.description, item.index)).join('');
+  }
 }
