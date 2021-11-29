ec85608fb5090229aa4aa7681a5e66fd3c786590
Anselem <anselemodims@gmail.com>
2021-11-29 12:11:29 +0100

    Created a formHandler function to handle submission of forms

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,17 @@
+/* eslint-disable class-methods-use-this */
 import Utils from './utils.js';
 
 const utils = new Utils();
 
 export default class Helper {
-
+  /**
+   * @function formHandler - instance method for handling of form submission
+   */
+  formHandler(e) {
+    e.preventDefault();
+    utils.add();
+    document.querySelector('.todo-list ul').innerHTML = '';
+    document.querySelector('.todo-list ul').insertAdjacentHTML('beforeend', utils.render());
+    utils.clear();
+  }
 }
