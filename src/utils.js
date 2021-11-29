735640769a58746a777f66e010f5432c2ee077c1
Anselem <anselemodims@gmail.com>
2021-11-29 16:37:56 +0100

    Tweaked the logic of the add and render helper functions

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 /* eslint-disable class-methods-use-this */
-import task from './tasks.js';
 import Component from './component.js';
 
 export default class Utils {
@@ -7,20 +6,19 @@ export default class Utils {
    * @function render - instance class method
    * @returns - a list element formed from the component
    */
-  render() {
-    return task.map((item) => Component.list(item.description, item.index)).join('');
+  render(val, id) {
+    document.querySelector('.todo-list ul').insertAdjacentHTML('beforeend', Component.list(val, id));
   }
 
   /**
    * @function add - instance method that adds new todo to list
    */
   add() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const todo = document.querySelector('#todo').value;
-    task.push({
-      description: `${todo}`,
-      completed: false,
-      index: task.length + 1,
-    });
+    this.pushControl(tasks, todo);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    this.render(todo, tasks.length);
   }
 
   /**
