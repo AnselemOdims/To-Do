52c769ced7239b7d79e28bf1a48574cc5122b8af
Anselem <anselemodims@gmail.com>
2021-11-29 12:15:50 +0100

    Added the fucntions to run when the document has finsihed loading

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import Utils from './utils.js';
 
 const helper = new Helper();
 const utils = new Utils();
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.todo-list ul').innerHTML = utils.render();
+  document.querySelector('form').addEventListener('submit', () => helper.formHandler());
+  document.querySelector('.enter').addEventListener('click', () => helper.formHandler());
+});
