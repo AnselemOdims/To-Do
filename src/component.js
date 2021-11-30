f864bbaa422d4f4db87b2b147737ee062169527a
Anselem <anselemodims@gmail.com>
2021-11-30 15:13:31 +0100

    Add the draggable attribute to the list component

--- src/component.js
+++ src/component.js
@@ -4,12 +4,14 @@ import check from './images/icons/check.svg';
 export default class Component {
   static list(val, id) {
     return `
-  <li class="list">
-  <input type="checkbox" id="check" data-id=${id}><img src=${check} alt="checked icon" class="d-none" data-id=${id}>
-  <input type="text" class="input-value" value='${val}'>
-  <a><i class="fas fa-ellipsis-v"></i></a>
-  <a class="d-none"  id="delete"><i class="far fa-trash-alt" data-id=${id}></i></a>
-</li>
+    <li class="list" draggable="true">
+    <input type="checkbox" id="check" data-id=${id}><img src=${check} alt="checked icon" class="d-none" data-id=${id}>
+    <form data-id=${id}>
+    <input type="text" class="input-value" value='${val}'>
+    </form>
+    <a><i class="fas fa-ellipsis-v drag"></i></a>
+    <a class="d-none"  id="delete"><i class="far fa-trash-alt" data-id=${id}></i></a>
+  </li>
   `;
   }
 }
