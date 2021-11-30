bb4426666dacdf1f2ce6312edb3c2ea489b37a2b
Anselem <anselemodims@gmail.com>
2021-11-30 16:34:14 +0100

    Implemented the rotate functionality

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('form').addEventListener('submit', (e) => Helper.formHandler(e));
   document.querySelector('.enter').addEventListener('click', (e) => Helper.formHandler(e));
   document.querySelector('.clear a').addEventListener('click', () => Utils.clearAll());
+  document.querySelector('#refresh .fas').addEventListener('click', (e) => {
+    e.target.classList.add('rotate');
+    setTimeout(() => { e.target.classList.remove('rotate'); }, 1000);
+  });
 });
