505df374ab95cd8c2dad016a8153b946b6336517
Anselem <anselemodims@gmail.com>
2021-12-15 15:19:00 +0100

    Feat: created the getDragElem static helper method

--- src/drag.js
+++ src/drag.js
@@ -1,7 +1,7 @@
 export default class Drag {
   /**
    * @static
-   * @function dragStartEnd
+   * @function dragStartEnd - handles the drag start and end
    * @memberof Drag
    */
   static dragStartEnd() {
@@ -13,5 +13,28 @@ export default class Drag {
         item.classList.remove('dragging');
       });
     });
+  }
+
+  /**
+   * @static
+   * @method getDragElem - helper method for the drag function
+   * @param {HTML ELement} container
+   * @param {Number} vertical - the y position of the client
+   * @returns element
+   */
+  static getDragElem(container, vertical) {
+    const dragElems = [...container.querySelectorAll('li.list:not(.dragging)')];
+    return dragElems.reduce((closest, elemChild) => {
+      const box = elemChild.getBoundingClientRect();
+      const offset = vertical - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return {
+          offset,
+          element: elemChild,
+        };
+      }
+
+      return closest;
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
   }
 }
