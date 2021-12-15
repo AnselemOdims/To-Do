5fc7383c0389fd5021a3e5653106f36ea3173582
Anselem <anselemodims@gmail.com>
2021-12-15 15:28:46 +0100

    Feat: imported drag function into necessary modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import Helper from './helpers.js';
 import Utils from './utils.js';
+import Drag from './drag.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   Helper.display();
+  Drag.dragFunc();
   document.querySelector('form').addEventListener('submit', (e) => Helper.formHandler(e));
   document.querySelector('.enter').addEventListener('click', (e) => Helper.formHandler(e));
   document.querySelector('.clear a').addEventListener('click', () => Utils.clearAll());
